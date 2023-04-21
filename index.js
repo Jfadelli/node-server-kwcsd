11f0ebb8691253cd23bf9a08dc0b7a469ce96237
Jason <jfadelli@gmail.com>
2023-04-21 02:05:43 -0700

    added cors part 5

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.use(bodyParser.json());
 app.use('/api/', router)
 app.use(cors())
 router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://kwcsandiego.com"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    process.env.NODE_ENV === "production" ? res.header("Access-Control-Allow-Origin", "https://kwcsandiego.com") : res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from;
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
