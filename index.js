a34032e22537e0fec198386ca695a3cc14301a25
Jason <jfadelli@gmail.com>
2023-04-21 00:55:35 -0700

    added cors part 2

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ app.use('/api/', router)
 app.use(cors({
     origin: "*"
 }))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 router.post('/send', function (req, res) {
+    req.headers['']
     let transporter = nodeMailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
