900ef5d30b0716cabb5bab3c482a726a3c652870
Jason <jfadelli@gmail.com>
2023-04-21 01:49:15 -0700

    added cors part 4

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api/', router)
 app.use(cors())
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://kwcsandiego.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 router.post('/send', function (req, res) {
     req.headers['']
